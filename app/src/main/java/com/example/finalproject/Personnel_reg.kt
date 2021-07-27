e5d3f4a3632ca1dc11702b3eaeadd32c3acae140
wlsskdbwls <wlsskdbwls@naver.com>
2021-07-27 16:52:40 +0900

    First commit

---
+++ app/src/main/java/com/example/finalproject/Personnel_reg.kt
@@ -0,0 +1,16 @@
+package com.example.finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Personnel_reg : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personnel_reg)
+    }
+
+    /*override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_reg, menu)
+        return true
+    }*/
+}
