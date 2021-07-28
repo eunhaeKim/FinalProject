3c6258ba55f645cbb7b459c20a9bfba0f96fc575
wlsskdbwls <wlsskdbwls@naver.com>
2021-07-29 00:04:45 +0900

    reg수정

--- app/src/main/java/com/example/finalproject/PersonnelReg.kt
+++ app/src/main/java/com/example/finalproject/PersonnelReg.kt
@@ -1,17 +1,70 @@
 package com.example.finalproject
 
+import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 
 class PersonnelReg : AppCompatActivity() {
+
+    lateinit var dbManager: DBManager
+    lateinit var sqlitedb: SQLiteDatabase
+
+    lateinit var btnRegister: Button
+    lateinit var edtDate: EditText
+    lateinit var edtCaffeine: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personnel_reg)
+
+        btnRegister = findViewById(R.id.btnRegister)
+        edtDate = findViewById(R.id.edtDate)
+        edtCaffeine = findViewById(R.id.edtCaffeine)
+
+        dbManager = DBManager(this, "personnelDB", null, 1)
+
+        btnRegister.setOnClickListener() {
+            var str_date: String = edtDate.text.toString()
+            var str_caffeine: String = edtCaffeine.text.toString()
+
+            sqlitedb = dbManager.writableDatabase
+            sqlitedb.execSQL("INSERT INTO personnel VALUES('" + str_date + "', " + str_caffeine + "')")
+            sqlitedb.close()
+
+            val intent = Intent(this, PersonnelInfo::class.java)
+            intent.putExtra("intent_date", str_date)
+            startActivity(intent)
+
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_reg, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.action_home -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+
+            R.id.action_list -> {
+                val intent = Intent(this, PersonnelList::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
